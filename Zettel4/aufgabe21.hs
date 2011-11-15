c132760b6f228079c8deb98dcd2902fc87b38cea
André Röhrig <andre.roehrig@gmail.com>
2011-11-15 12:40:34 +0100

    aufgabe21 solala

---
+++ Zettel4/aufgabe21.hs
@@ -0,0 +1,5 @@
+zinsen :: Double -> Double -> Double
+zinsen kapital zinsfuß = kapital * zinsfuß * 0.01
+
+--funktion :: [Double]->[Double]
+--die 2.25 sind der zinsfuß, die liste dahinter die kapitale
