d67619e52ad31667da2d1d02d2e3aa3af1935d98
André Röhrig <andre.roehrig@gmail.com>
2011-12-12 00:31:14 +0100

    Aufgabe43 complete

---
+++ Zettel7/aufgabe43.hs
@@ -0,0 +1,16 @@
+comparelistsum :: [Integer] -> [Integer] -> [Integer] 
+comparelistsum xs ys = if (sum xs) >= (sum ys) then xs
+                else ys
+
+recursionbaby :: [Integer] -> [Integer] -> [Integer]
+recursionbaby xs [] = xs
+recursionbaby xs ys = if comparelistsum xs ys == xs then recursionbaby xs (tail ys)
+                    else recursionbaby ys (tail ys)
+
+findsubseq :: [Integer] -> [Integer] -> [Integer]
+findsubseq xs [] = xs
+findsubseq xs ys = if sum (recursionbaby xs (tail xs)) >= sum (recursionbaby ys (tail ys)) then findsubseq xs (init ys)
+                 else findsubseq ys (init ys)
+
+bigsubseq :: [Integer] -> Integer
+bigsubseq xs = sum(findsubseq xs (init xs))
